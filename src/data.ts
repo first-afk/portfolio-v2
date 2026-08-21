6e761f668b2a607937da0f70cf57cc715ce963ed
first-afk <oriejiestherr@gmail.com>
2026-08-21 21:59:31 +0100

    feat: adds animated background to hero section

---
+++ src/data.ts
@@ -0,0 +1,15 @@
+interface testimonialProps {
+  id: number;
+  message: string;
+  name: string;
+  title: string;
+}
+
+export const testimonial: testimonialProps[] = [
+  { id: 1, message: "", name: "", title: "" },
+  { id: 2, message: "", name: "", title: "" },
+  { id: 3, message: "", name: "", title: "" },
+  { id: 4, message: "", name: "", title: "" },
+  { id: 5, message: "", name: "", title: "" },
+  { id: 6, message: "", name: "", title: "" },
+];
