a1f13f6cbc36b5ed98054ed39de2b2e86944c0f6
first-afk <oriejiestherr@gmail.com>
2026-08-27 11:20:41 +0100

    feat: adds work cards

---
+++ src/components/card.tsx
@@ -0,0 +1,72 @@
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+  bg?: string;
+  image?: string;
+  id?: string;
+  category?: string;
+  title?: string;
+  description?: string;
+  url?: string;
+}
+
+export function Card({
+  bg,
+  image,
+  id,
+  category,
+  title,
+  description,
+  url,
+  className,
+  children,
+}: CardProps) {
+  return (
+    <article
+      className={`work-card ${bg} relative flex  items-center justify-center ${className} shadow-2xl`}
+    >
+      <img
+        src="/images/tape2.png"
+        className="tape-graphic h-12 w-50"
+        alt="tape"
+      />
+      {id && (
+        <div>
+          <div
+            className={`relative flex flex-col items-center gap-4 py-10 text-center`}
+          >
+            <p className="self-end font-playfair italic text-lg tracking-wide px-2">
+              {id}
+            </p>
+
+            <div className="torn-image-container w-full h-fit px-4 -mt-2">
+              <img
+                src={`/images/${image}`}
+                alt=""
+                className="torn-image h-36 w-64 "
+              />
+            </div>
+
+            <div className="flex flex-col items-start justify-start px-6 py-6">
+              <p className="font-mono text-xs uppercase tracking-[0.2em]">
+                {category}
+              </p>
+              <h3 className="font-playfair text-3xl italic">{title}</h3>
+              <p className="max-w-xs text-sm leading-relaxed text-left">
+                {description}
+              </p>
+            </div>
+            <hr className="my-2 w-full border-current/30" />
+            <p className="mt-auto">
+              <a
+                className="font-mono text-xs uppercase tracking-widest underline underline-offset-4"
+                href={url}
+              >
+                View case &rarr;
+              </a>
+            </p>
+          </div>
+        </div>
+      )}
+      {children}
+    </article>
+  );
+}
--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -1,15 +1,20 @@
+import { Card } from "../components/card";
+
 const Works = () => {
   return (
-    <section
-      id="works"
-      className="relative z-20 flex min-h-dvh px-10 py-10 flex-col"
-    >
-      <h1 className="font-mono whitespace-nowrap overflow-hidden border-r-2 w-0 tracking-wider title h-fit">
+    <section id="works" className="relative z-20 flex min-h-dvh py-10 flex-col">
+      <h1 className="font-mono whitespace-nowrap overflow-hidden border-r-2 w-0 tracking-wider title h-fit px-10">
         ~/esther/works
       </h1>
-
-      <div className="space-y-16 flex max-md:flex-col">
-        <div className="md:w-1/2 w-2/3">
+      <div className="absolute md:block hidden">
+        <img
+          src="/images/name.png"
+          alt="display-image"
+          className="rotate-80 -translate-x-40 translate-y-24 size-3/5"
+        />
+      </div>
+      <div className="space-y-16 flex max-md:flex-col z-20 px-10 py-10 w-full">
+        <div className="max-md:w-2/3 w-1/2 md:px-18">
           <h2 className="heading-h1 font-playfair leading-[1.12] py-5 tracking-[0.02em] md:w-2/3">
             things i've created.
           </h2>
@@ -19,19 +24,67 @@ const Works = () => {
           </p>
         </div>
 
-        <div className="scrapbook-wrapper flex items-center justify-center">
-          <img
-            src="/images/tape2.png"
-            className="tape-graphic h-15 w-50"
-            alt="tape"
-          />
+        <div className="grid min-w-0 grid-cols-1 gap-x-5 gap-y-16 md:grid-cols-2 p-2 md:w-1/2">
+          <div className="-rotate-2 md:-translate-x-4 md:-translate-y-10 ">
+            <Card
+              bg="bg-lime-200"
+              image="name.png"
+              id="01"
+              category="health"
+              title="optonews"
+              description="lorem ipsum set dolor met kini kor kini kor"
+              url=""
+            />
+          </div>
+          <div className="rotate-3 md:-translate-x-0.5 md:translate-y-10 scrapbook-wrapper">
+            <img
+              src="/images/tape2.png"
+              className="tape-graphic h-12 w-50"
+              alt="tape"
+            />
+            <Card
+              bg="bg-mauve-200"
+              image="name.png"
+              id="01"
+              category="health"
+              title="optonews"
+              description="lorem ipsum set dolor met kini kor kini kor"
+              url=""
+              className="torn-paper-container"
+            />
+          </div>
+          <div className="rotate-2 md:-translate-x-2">
+            <Card
+              bg="bg-mist-300"
+              image="name.png"
+              id="01"
+              category="health"
+              title="optonews"
+              description="lorem ipsum set dolor met kini kor kini kor"
+              url=""
+            />
+          </div>
+          <div className="-rotate-2 md:-translate-x-0.5 md:translate-y-24 ">
+            <Card
+              bg="bg-fuchsia-200"
+              image="name.png"
+              id="01"
+              category="health"
+              title="optonews"
+              description="lorem ipsum set dolor met kini kor kini kor"
+              url=""
+            />
+          </div>
 
-          {/* <img src="paperclip.png" className="clip-graphic" alt="paperclip" /> */}
-
-          <div className="torn-paper-container bg-mauve-200 flex items-center justify-center">
-            <div className="torn-image-container">
-              <img src="/images/name.png" alt="" className="torn-image" />
-            </div>
+          <div className="items-center justify-center flex ">
+            <Card
+              bg="bg-amber-300"
+              className="h-36 w-fit items-center justify-center flex p-2"
+            >
+              <p className="w-2/3 leading-[1.38] text-2xl font-normal font-caveat">
+                Currently building something cool ^_~
+              </p>
+            </Card>
           </div>
         </div>
       </div>
